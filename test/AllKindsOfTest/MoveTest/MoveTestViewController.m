79f8e37f108993a09e38ff3faf222a520433a4a5
宋锡铭 <sven.song@qq.com>
2019-04-29 10:14:06 +0800

    变字号test

--- test/AllKindsOfTest/MoveTest/MoveTestViewController.m
+++ test/AllKindsOfTest/MoveTest/MoveTestViewController.m
@@ -10,6 +10,7 @@
 
 @interface MoveTestViewController ()
 @property (nonatomic, strong) UILabel *moveLb;
+@property (nonatomic, strong) UILabel *inerLb;
 @end
 
 @implementation MoveTestViewController
@@ -20,7 +21,7 @@
 }
 
 - (void)initUI {
-    // UIView的动画是没问题的,用label宽度就会突然变化. 
+    // UIView的动画是没问题的,用label宽度就会突然变化.
     UIView *label = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, [UIFont systemFontOfSize:20].lineHeight)];
 //    label.font = [UIFont systemFontOfSize:20];
     [self.view addSubview:label];
@@ -32,7 +33,18 @@
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(moveLabel)];
     [label addGestureRecognizer:tap];
     
+    
+    UILabel *inerLb = [[UILabel alloc] initWithFrame:CGRectMake(20, 0, 60, [UIFont systemFontOfSize:20].lineHeight)];
+    [label addSubview:inerLb];
+    inerLb.font = [UIFont systemFontOfSize:12];
+    inerLb.textAlignment = NSTextAlignmentCenter;
+    inerLb.text = @"h";
+    self.inerLb = inerLb;
+    
+//    [self buttonTest];
 }
+
+
 
 - (void)moveLabel {
     
@@ -40,6 +52,11 @@
     [UIView animateWithDuration:0.5 animations:^{
         self.moveLb.frame = CGRectMake(100, 200, 50, [UIFont systemFontOfSize:20].lineHeight);
     }];
+}
+
+-(void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.inerLb.center = CGPointMake(self.moveLb.frame.size.width * 0.5, self.moveLb.frame.size.height * 0.5);
 }
 
 @end
