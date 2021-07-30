5e73b0c08b9cbdf86b113cb42cff57cd276cb695
宋锡铭 <songximing@guixue.com>
2021-07-30 11:16:21 +0800

    copy和mutableCopy

--- test/AllKindsOfTest/CopyStrong/CopyStrongTestViewController.m
+++ test/AllKindsOfTest/CopyStrong/CopyStrongTestViewController.m
@@ -52,17 +52,20 @@
     // 证书收费,免费
     
     
-    [self dicTest];
+    //    [self dicTest];
+    
+    
+    [self deepSwill];
 }
 
 
 - (void)dicTest { // 2:00
     
-//    @property (copy) NSDictionary *dicCopy; // 外部变化,不会变
-
-//    @property (strong) NSDictionary *strongDic; // 外部变化,会变
+    //    @property (copy) NSDictionary *dicCopy; // 外部变化,不会变
     
-//    @property (copy) NSMutableDictionary *mutaDic; // 外部变化, 不会变
+    //    @property (strong) NSDictionary *strongDic; // 外部变化,会变
+    
+    //    @property (copy) NSMutableDictionary *mutaDic; // 外部变化, 不会变
     
     NSLog(@"-------------------------------");
     NSDictionary *dic = @{
@@ -78,6 +81,47 @@
     [mDic setValue:@"99" forKey:@"key1"];
     
     NSLog(@"copyDic:%@--strongDic:%@--mutDic:%@",self.dicCopy, self.strongDic,self.mutaDic);
+}
+
+
+// 深拷贝浅拷贝测试
+- (void)deepSwill {
+    NSMutableString * str1 =  [NSMutableString stringWithString:@"Bian"] ;
+    NSMutableString * str2 = [NSMutableString stringWithString:@"Sun"] ;
+    NSMutableArray * mutableArr = [[NSMutableArray alloc] initWithObjects:str1,str2, nil];
+    
+    // 修改str1的值
+    [str1 insertString:@"abc" atIndex:0];
+    
+    
+    //mutableArr 是可变还是不可变? 我觉得应该是可变,因为是可变初始化的啊
+    
+    NSLog(@"mutableArr类型:%@",[mutableArr class]);
+    
+    // str1和 mutableArr 首元素值一样吗? 值是一样的,因为原始str1是可变字符串
+    
+    NSLog(@"str1:%@--集合里面%@",str1,mutableArr[0]);
+}
+
+
+- (void)deepShallow {
+    NSMutableString * str1 =  [NSMutableString stringWithString:@"Bian"] ;
+   NSMutableString * str2 = [NSMutableString stringWithString:@"Sun"] ;
+   NSMutableArray * mutableArr = [[NSMutableArray alloc] initWithObjects:str1,str2, nil];
+   NSMutableArray * copyMutableArr = [mutableArr mutableCopy];
+
+    // a, b, c
+   NSLog(@"mutableArr:%p %p %p",mutableArr,mutableArr[0],mutableArr[1]);
+    // d, b, c
+   NSLog(@"copyMutableArr:%p %p %p",copyMutableArr,copyMutableArr[0],copyMutableArr[1]);
+    // 修改str1的值
+    [str1 insertString:@"abc" atIndex:0];
+    // a, b, c
+   NSLog(@"mutableArr:%p %p %p",mutableArr,mutableArr[0],mutableArr[1]);
+    // d, b, c
+   NSLog(@"copyMutableArr:%p %p %p",copyMutableArr,copyMutableArr[0],copyMutableArr[1]);
+    // abcBian
+   NSLog(@"%@",copyMutableArr[0]);
 }
 
 @end
